c314ca0a980b0e966a77103052ae68c6e33c450e
Aatoken <3410760440@qq.com>
2019-04-23 23:24:47 +0800

    '1.Ascll的使用2.Kotlin中with的高级用法'

---
+++ src/main/kotlin/Ascll.kt
@@ -0,0 +1,44 @@
+import java.lang.StringBuilder
+
+/**
+ * Auther Aatoken
+ * Date 2019/4/23
+ * Des Ascll学习
+ */
+
+
+fun main(args: Array<String>) {
+
+    val c = 'a'
+
+    val value = c.toInt()
+
+    println(value)
+
+
+    val str = "I Love You"
+    val array = str.toCharArray()
+
+    //val stringBuilder = StringBuilder()
+
+    //1.
+//    for(cha in array)
+//    {
+//        stringBuilder.append(cha.toInt().toString()+" ")
+//    }
+//
+//    println(stringBuilder.toString())
+
+    //2.
+    val result = with(StringBuilder())
+    {
+        for (cha in array) {
+            append(cha.toInt().toString() + " ")
+        }
+
+        toString()
+    }
+
+    println(result)
+
+}
